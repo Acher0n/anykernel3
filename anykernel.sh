d335ab791af86a5d62293107b6f08fec75fd2eb2
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-08 23:35:29 +0900

    Patch kernel binary for Magisk
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -11,6 +11,15 @@ eval $(cat /tmp/anykernel/props | grep -v '\.')
 ## AnyKernel install
 dump_boot;
 
+# Change skip_initramfs to want_initramfs if Magisk is detected
+if [ -d $ramdisk/.backup ]; then
+  ui_print " ";
+  ui_print "Magisk detected!";
+  ui_print "Patching kernel so that reflashing Magisk is not necessary...";
+  gzip -dc < /tmp/anykernel/Image.gz | sed -e 's/skip_initramfs/want_initramfs/g' | gzip > /tmp/anykernel/Image.gz.tmp;
+  mv /tmp/anykernel/Image.gz.tmp /tmp/anykernel/Image.gz;
+fi
+
 # Install the boot image
 write_boot;
 
